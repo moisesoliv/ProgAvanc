f56be911c4627464323300616bb0390f6c33ba8b
damiao <giuliano.dmaiao@hotmail.com>
2017-10-03 15:02:25 -0300

    2 funcoes

---
+++ grupo1/view.py
@@ -0,0 +1,12 @@
+
+
+class View():
+	def __init__(self, saque):
+		self.saque = saque
+
+	def mensagem(self):
+		print("Catão Inserido")
+
+	def campodesenha(self):
+		a = str(raw_input("Insira sua senha: "))
+		
