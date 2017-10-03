ab512d5f351711c141667ba29eb6527243fcc5d2
damiao <giuliano.dmaiao@hotmail.com>
2017-10-03 15:48:37 -0300

    Novas fincoes

--- grupo1/view.py
+++ grupo1/view.py
@@ -1,12 +1,22 @@
 
 
 class View():
-	def __init__(self, saque):
-		self.saque = saque
-
-	def mensagem(self):
-		print("Cartão Inserido")
+	def __init__(self):
+		pass
+		
+	def mensagemcartao(self, bool cartao):
+		if cartao == 1:
+			print("Cartão Inserido")
+		else:
+			print("Insira o cartão")
 
 	def campodesenha(self):
-		a = str(raw_input("Insira sua senha: "))
-		
+		self.a = str(raw_input("Insira sua senha: "))
+
+	def mensagemsenha(self, senha):
+		if self.a != senha:
+			print("Senha Incorreta")
+
+	def botaoSaldo(self):
+		return True
+			
